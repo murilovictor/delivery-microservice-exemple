d0dea56c79329b93a653933d1ef8f8ec03450501
Murilo Victor <murilo.victor@unicid.edu.br>
2020-09-28 17:16:41 -0300

    Criando o Config-Server

---
+++ delivery-config-server/src/main/java/com/mvictor/delivery/DeliveryConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.mvictor.delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryConfigServerApplication.class, args);
+	}
+
+}
